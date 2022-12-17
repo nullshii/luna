126ecad6054182162aa9e75af2a68dc9ffe5b719
Ray <60722967+nullshii@users.noreply.github.com>
2022-12-17 19:11:43 +0400

    test stuff

--- game/src/main.c
+++ game/src/main.c
@@ -2,6 +2,9 @@
 #include <core/asserts.h>
 #include <core/logger.h>
 
+// ! Temporary
+#include <platform/platform.h>
+
 int main()
 {
 	LFATAL("Pi is: %f", 3.14f);
@@ -10,8 +13,18 @@ int main()
 	LDEBUG("Pi is: %f", 3.14f);
 	LTRACE("Pi is: %f", 3.14f);
 
-	LASSERT(TRUE);
-	LASSERT_MSG(FALSE, "BOBA");
+	// LASSERT(TRUE);
+	// LASSERT_MSG(FALSE, "BOBA");
+
+	platform_state state;
+	if (platform_startup(&state, "Luna Engine", 20, 20, 1280, 720))
+	{
+		while (TRUE)
+		{
+			platform_pump_message(&state);
+		}
+	}
+	platform_shutdown(&state);
 
 	return 0;
 }
