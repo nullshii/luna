3db1c49ffb1fc9b0cd1d0ba8e96fc12a62eba718
Ray <60722967+nullshii@users.noreply.github.com>
2022-12-17 19:10:27 +0400

    Removed platform definition from code

--- luna/src/defines.h
+++ luna/src/defines.h
@@ -45,42 +45,10 @@ STATIC_ASSERT(sizeof(f64) == 8, "Expected f64 to be 8 bytes.");
 #define TRUE 1
 #define FALSE 0
 
-// Platform detection
-#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__)
-#define LPLATFORM_WINDOWS 1
+#ifdef LUNA_PLATFORM_WINDOWS
 #ifndef _WIN64
 #error "64-bit is required on Windows!"
 #endif
-#elif defined(__linux__) || defined(__gnu_linux__)
-// Linux OS
-#define LPLATFORM_LINUX 1
-#if defined(__ANDROID__)
-#define LPLATFORM_ANDROID 1
-#endif
-#elif defined(__unix__)
-// Catch anything not caught by the above.
-#define LPLATFORM_UNIX 1
-#elif defined(_POSIX_VERSION)
-// Posix
-#define LPLATFORM_POSIX 1
-#elif __APPLE__
-// Apple platforms
-#define LPLATFORM_APPLE 1
-#include <TargetConditionals.h>
-#if TARGET_IPHONE_SIMULATOR
-// iOS Simulator
-#define LPLATFORM_IOS 1
-#define LPLATFORM_IOS_SIMULATOR 1
-#elif TARGET_OS_IPHONE
-#define LPLATFORM_IOS 1
-// iOS device
-#elif TARGET_OS_MAC
-// Other kinds of Mac OS
-#else
-#error "Unknown Apple platform"
-#endif
-#else
-#error "Unknown platform!"
 #endif
 
 #ifdef LEXPORT
