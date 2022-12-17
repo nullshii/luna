06178d8f6863adba91efbdd0aa994dabc039fcf3
Ray <60722967+nullshii@users.noreply.github.com>
2022-12-17 19:10:43 +0400

    style...

--- luna/src/core/asserts.h
+++ luna/src/core/asserts.h
@@ -15,41 +15,41 @@
 
 LAPI void report_assertion_failure(const char* expression, const char* message, const char* file, i32 line);
 
-#define LASSERT(expression)                                                         \
-	{                                                                               \
-		if (expression)                                                             \
-			{                                                                       \
-			}                                                                       \
-		else                                                                        \
-			{                                                                       \
-				report_assertion_failure(#expression, "empty", __FILE__, __LINE__); \
-				debugbreak();                                                       \
-			}                                                                       \
+#define LASSERT(expression)                                                     \
+	{                                                                           \
+		if (expression)                                                         \
+		{                                                                       \
+		}                                                                       \
+		else                                                                    \
+		{                                                                       \
+			report_assertion_failure(#expression, "empty", __FILE__, __LINE__); \
+			debugbreak();                                                       \
+		}                                                                       \
 	}
 
-#define LASSERT_MSG(expression, message)                                            \
-	{                                                                               \
-		if (expression)                                                             \
-			{                                                                       \
-			}                                                                       \
-		else                                                                        \
-			{                                                                       \
-				report_assertion_failure(#expression, message, __FILE__, __LINE__); \
-				debugbreak();                                                       \
-			}                                                                       \
+#define LASSERT_MSG(expression, message)                                        \
+	{                                                                           \
+		if (expression)                                                         \
+		{                                                                       \
+		}                                                                       \
+		else                                                                    \
+		{                                                                       \
+			report_assertion_failure(#expression, message, __FILE__, __LINE__); \
+			debugbreak();                                                       \
+		}                                                                       \
 	}
 
 #ifdef LUNA_BUILD_MODE_DEBUG
-#define LASSERT_DEBUG(expression)                                                   \
-	{                                                                               \
-		if (expression)                                                             \
-			{                                                                       \
-			}                                                                       \
-		else                                                                        \
-			{                                                                       \
-				report_assertion_failure(#expression, "empty", __FILE__, __LINE__); \
-				debugbreak();                                                       \
-			}                                                                       \
+#define LASSERT_DEBUG(expression)                                               \
+	{                                                                           \
+		if (expression)                                                         \
+		{                                                                       \
+		}                                                                       \
+		else                                                                    \
+		{                                                                       \
+			report_assertion_failure(#expression, "empty", __FILE__, __LINE__); \
+			debugbreak();                                                       \
+		}                                                                       \
 	}
 #else
 #define LASSERT_DEBUG(expression)
